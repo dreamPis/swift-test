96204ccc25c4d02742efb1b63602c4a7831347e2
d[one]rem <329564909@qq.com>
2018-05-22 15:07:42 +0800

    Initial Commit

---
+++ Lottery_Ticket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lottery_Ticket
+//
+//  Created by 段林江 on 2018/5/22.
+//  Copyright © 2018年 段林江. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
